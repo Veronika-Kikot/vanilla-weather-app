b34330b7b852f933aab1b5115e166ef29499d469
Veronika-Kikot <khomenkovl14@gmail.com>
2022-09-18 16:41:52 +0300

    Added units conversion

--- src/index.js
+++ src/index.js
@@ -25,7 +25,8 @@ return `${day} ${hours}:${minutes}`
 
 function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    celciusTemperature = response.data.main.temp;
+    temperatureElement.innerHTML = Math.round(celciusTemperature);
     let cityELement = document.querySelector("#city");
     cityELement.innerHTML = response.data.name;
     humidityElement.innerHTML = response.data.main.humidity;
@@ -68,3 +69,31 @@ function currentLocation(event) {
 
 let currentBtn = document.querySelector("#current");
 currentBtn.addEventListener("click", currentLocation);
+
+
+// Convert celcius to far
+
+function convertToFar(event){
+event.preventDefault();
+celc.classList.remove("active");
+farLink.classList.add("active");
+let temperatureElement = document.querySelector("#temperature");
+let far = Math.round((celciusTemperature * 9) / 5 + 32);
+temperatureElement.innerHTML = far;
+}
+
+function convertToCelc(event){
+event.preventDefault();
+celc.classList.add("active");
+farLink.classList.remove("active");
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = Math.round(celciusTemperature);
+}
+
+let celciusTemperature = null;
+
+let farLink = document.querySelector("#far-link");
+farLink.addEventListener("click", convertToFar)
+
+let celc = document.querySelector("#celc-link");
+celc.addEventListener("click", convertToCelc)
