312baf5a0e2f1de395d8dc7232397359d92ac62c
Veronika-Kikot <khomenkovl14@gmail.com>
2022-09-26 21:24:34 +0300

    remove F unit

--- src/index.js
+++ src/index.js
@@ -44,8 +44,8 @@ function displayForecast(response){
                         </div>
                         <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42px">
                         <div class="weather-forecast-temperatures">
-                            <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°C</span>
-                            <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°C</span>
+                            <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+                            <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
                         </div>
                     </div>`;
         }
@@ -111,27 +111,27 @@ currentBtn.addEventListener("click", currentLocation);
 
 // Convert celcius to far
 
-function convertToFar(event){
-event.preventDefault();
-celc.classList.remove("active");
-farLink.classList.add("active");
-let temperatureElement = document.querySelector("#temperature");
-let far = Math.round((celciusTemperature * 9) / 5 + 32);
-temperatureElement.innerHTML = far;
-}
+// function convertToFar(event){
+// event.preventDefault();
+// celc.classList.remove("active");
+// farLink.classList.add("active");
+// let temperatureElement = document.querySelector("#temperature");
+// let far = Math.round((celciusTemperature * 9) / 5 + 32);
+// temperatureElement.innerHTML = far;
+// }
 
-function convertToCelc(event){
-event.preventDefault();
-celc.classList.add("active");
-farLink.classList.remove("active");
-let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHTML = Math.round(celciusTemperature);
-}
+// function convertToCelc(event){
+// event.preventDefault();
+// celc.classList.add("active");
+// farLink.classList.remove("active");
+// let temperatureElement = document.querySelector("#temperature");
+// temperatureElement.innerHTML = Math.round(celciusTemperature);
+// }
 
-let celciusTemperature = null;
+// let celciusTemperature = null;
 
-let farLink = document.querySelector("#far-link");
-farLink.addEventListener("click", convertToFar)
+// let farLink = document.querySelector("#far-link");
+// farLink.addEventListener("click", convertToFar)
 
-let celc = document.querySelector("#celc-link");
-celc.addEventListener("click", convertToCelc)
+// let celc = document.querySelector("#celc-link");
+// celc.addEventListener("click", convertToCelc)
